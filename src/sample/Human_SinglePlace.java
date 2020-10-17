c79026d9954b353725780aa88f826de0ffc05fa3
wzp21142 <411314320@qq.com>
2020-10-18 00:20:05 +0800

    Fix Human_SinglePlace.

--- src/sample/Human_SinglePlace.java
+++ src/sample/Human_SinglePlace.java
@@ -27,6 +27,103 @@ public class Human_SinglePlace {
         this.IllLevel = new SimpleStringProperty(IllLevel);
         this.arrivetime = new SimpleStringProperty(arrivetime);
         this.leavetime = new SimpleStringProperty(leavetime);
+        ;
+    }
+
+    public String getArrivetime() {
+        return arrivetime.get();
+    }
+
+    public StringProperty arrivetimeProperty() {
+        return arrivetime;
+    }
+
+    public void setArrivetime(String arrivetime) {
+        this.arrivetime.set(arrivetime);
+    }
+
+    public String getLeavetime() {
+        return leavetime.get();
+    }
+
+    public StringProperty leavetimeProperty() {
+        return leavetime;
+    }
+
+    public void setLeavetime(String leavetime) {
+        this.leavetime.set(leavetime);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getWorkspace() {
+        return workspace.get();
+    }
+
+    public StringProperty workspaceProperty() {
+        return workspace;
+    }
+
+    public void setWorkspace(String workspace) {
+        this.workspace.set(workspace);
+    }
+
+    public String getHome() {
+        return home.get();
+    }
+
+    public StringProperty homeProperty() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home.set(home);
+    }
+
+    public String getPhonenumber() {
+        return phonenumber.get();
+    }
+
+    public StringProperty phonenumberProperty() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber.set(phonenumber);
+    }
+
+    public String getIsMaskOn() {
+        return isMaskOn.get();
+    }
+
+    public StringProperty isMaskOnProperty() {
+        return isMaskOn;
+    }
+
+    public void setIsMaskOn(String isMaskOn) {
+        this.isMaskOn.set(isMaskOn);
+    }
+
+    public String getIllLevel() {
+        return IllLevel.get();
+    }
+
+    public StringProperty illLevelProperty() {
+        return IllLevel;
+    }
+
+    public void setIllLevel(String illLevel) {
+        this.IllLevel.set(illLevel);
     }
 
     public String toString() {
